36c0bd39764e22936a07983c83281aa870f3505e
peejay6459 <philip.j.o.sepulveda@gmail.com>
2016-10-21 15:50:34 +1300

    Sample commit

--- app/src/main/java/com/example/jerome/niche/Settings.java
+++ app/src/main/java/com/example/jerome/niche/Settings.java
@@ -16,5 +16,6 @@ public interface Settings {
     void changeField(final TextView username, final TextView email, final TextView password, final TextView cPassword, final String... params);
     void changePasswordField(final TextView password, final TextView username, final String... params);
     void changePasswordField(final TextView username, final TextView email, final TextView password, final TextView cPassword, final String... params);
-    */
+
+     */
 }
