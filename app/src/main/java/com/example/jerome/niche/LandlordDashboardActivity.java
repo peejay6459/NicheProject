095deb0d9e2082326fdcd76a22246f8b6e291845
peejay6459 <philip.j.o.sepulveda@gmail.com>
2016-10-21 23:39:57 +1300

    Added XMLand java file for LandlordDashboard and code for AndroidManifest for LandlordDashboard

---
+++ app/src/main/java/com/example/jerome/niche/LandlordDashboardActivity.java
@@ -0,0 +1,18 @@
+package com.example.jerome.niche;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by philip on 10/21/2016.
+ */
+
+public class LandlordDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landlord_dashboard);
+        setTitle("Landlord Dashboard");
+    }
+}
