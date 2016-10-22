a0fb78542dc27aae33ad2fec2cdf917a7df239d0
peejay6459 <philip.j.o.sepulveda@gmail.com>
2016-10-22 20:47:17 +1300

    Added PropertyManager Child Activities(Personal Info, Manager Rooms, Review Tenants)

--- app/src/main/java/com/example/jerome/niche/Activities/PropertyManagerDashboardActivity.java
+++ app/src/main/java/com/example/jerome/niche/Activities/PropertyManagerDashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.jerome.niche.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TabHost;
 
 import com.example.jerome.niche.R;
 
@@ -10,11 +14,40 @@ import com.example.jerome.niche.R;
  */
 
 public class PropertyManagerDashboardActivity extends AppCompatActivity {
+    Button btnPropertyManagerInfo;
+    TabHost th;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_manager_dashboard);
         setTitle("Property Manager Dashboard");
+
+        btnPropertyManagerInfo = (Button) findViewById(R.id.btnPropertyManagerPersonalInformation);
+
+        th = (TabHost) findViewById(R.id.tvTabHost);
+        th.setup();
+        TabHost.TabSpec specs = th.newTabSpec("tag1");
+        specs.setContent(R.id.tab1);
+        specs.setIndicator("Dashboard");
+        th.addTab(specs);
+        specs = th.newTabSpec("tag2");
+        specs.setContent(R.id.tab2);
+        specs.setIndicator("News Feed");
+        th.addTab(specs);
     }
+
+    public void btnPropertyManagerPersonalInformation(View view)
+    {
+        this.startActivity(new Intent(this, PropertyManagerPersonalInformationActivity.class));
+    }
+    public void btnPropertyManagerManageRooms(View view)
+    {
+        this.startActivity(new Intent(this, PropertyManagerManageRoomsActivity.class));
+    }
+    public void btnPropertyManagerReviewTenants(View view)
+    {
+        this.startActivity(new Intent(this, PropertyManagerReviewTenantsActivity.class));
+    }
+
 }
---
+++ app/src/main/java/com/example/jerome/niche/activities/PropertyManagerReviewTenantsActivity.java
@@ -0,0 +1,15 @@
+package com.example.jerome.niche.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jerome.niche.R;
+
+public class PropertyManagerReviewTenantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property_manager_review_tenants);
+    }
+}
